9ff3945e74359b27acce6ddf6bdb64d8f3ac94c0
Cristi <pcristi1993@gmail.com>
2019-05-12 00:03:08 +0300

    converted to tflite

--- Convert.py
+++ Convert.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-converter = tf.contrib.lite.TFLiteConverter.from_saved_model('/home/cpop/Documents/1556363640', signature_key='predict')
+converter = tf.contrib.lite.TFLiteConverter.from_saved_model('/home/cpop/Documents/1557607409', signature_key='predict')
 tflite_model = converter.convert()
 open("converted_model.tflite", "wb").write(tflite_model)
